f742f7bd02c0eb4b4e1523629a553dd4d9cd12f4
Sid Mane <siddharthmane@Sids-MacBook-Pro.local>
2020-11-28 21:30:45 -0700

    Gaussian HMM w/ display

--- MM.py
+++ MM.py
@@ -14,10 +14,10 @@ import matplotlib.pyplot as plt
 class MM(ForecastingModel):
 	def model(self, zero_data, covariates):
 		duration = zero_data.size(-2)
-		init_dist = dist.Normal(0,10).expand([1]).to_event(1)
-		obs_dist = dist.Normal(0,10).expand([1]).to_event(1)
+		init_dist = dist.Normal(0,1).expand([1]).to_event(1)
+		obs_dist = dist.Normal(0,2).expand([1]).to_event(1)
 		obs_matrix = torch.tensor([[1.]])
-		trans_dist = dist.Normal(0,10).expand([1]).to_event(1)
+		trans_dist = dist.Normal(0,1).expand([1]).to_event(1)
 		trans_matrix = torch.tensor([[1.]])
 		pre_dist = dist.GaussianHMM(init_dist,trans_matrix, trans_dist, obs_matrix, obs_dist, duration=duration)
 		prediction = periodic_repeat(torch.zeros(1, 1), duration, dim=-1).unsqueeze(-1)
@@ -30,7 +30,7 @@ def main():
 	pyro.set_rng_seed(1)
 	pyro.clear_param_store()
 	covariates = torch.zeros(len(data), 0)	
-	forecaster = Forecaster(MM(), data[:700], covariates[:700], learning_rate=0.001)
+	forecaster = Forecaster(MM(), data[:700], covariates[:700], learning_rate=0.1, num_steps=400)
 	for name, value in forecaster.guide.median().items():
     		if value.numel() == 1:
         		print("{} = {:0.4g}".format(name, value.item()))
@@ -39,13 +39,11 @@ def main():
 	p10, p50, p90 = quantile(samples, (0.1, 0.5, 0.9)).squeeze(-1)
 	crps = eval_crps(samples, data[700:])
 	plt.figure(figsize=(9, 3))
+	plt.fill_between(torch.arange(700, 1404), p10, p90, color="red", alpha=0.3)
 	plt.plot(torch.arange(700, 1404), p50, 'r-', label='forecast')
 	plt.plot(torch.arange(700, 1404), data[700 : 1404], 'k-', label='truth')
-	plt.title("Total hourly ridership (CRPS = {:0.3g})".format(crps))
-	plt.ylabel("log(# rides)")
-	plt.xlabel("Hour after 2011-01-01")
 	plt.xlim(700, 1404)
 	plt.legend(loc="best");
-
+	plt.show()
 if __name__ == '__main__':
 	main()		
